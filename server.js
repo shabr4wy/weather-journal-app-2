39aea81dfdb750249bd3f91e6ca9bde6d07c2646
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-09-27 03:26:03 +0200

    set dependancies and server

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
+//depandancies
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port =4444;
+
+const Server = app.listen(port,() => {
+    console.log (`server is running: ${port}`);
+})
