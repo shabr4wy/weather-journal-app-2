e095a0c8301c6c330a3c57772fdf1ef3709952b7
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-09-28 01:26:47 +0200

    build function to get temperature

--- website/app.js
+++ website/app.js
@@ -1,5 +1,24 @@
 /* Global Variables */
+const apiKey = '5fce3a19bfe15beed4fcc55b9d25f811';
+const btn = document.querySelector('#generate');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+btn.addEventListener('click', async () => {
+    const zip = document.querySelector('#zip').value;
+    const feeling = document.querySelector('#feelings').value;
+
+    await getTemp(zip)
+
+});
+
+// building async function to fetch temperature data via api.
+async function getTemp(zip) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=metric`;
+    const weatherData = await fetch (url);
+    const res = await weatherData.json();
+    const temp = res.main.temp
+    return temp;
+}
